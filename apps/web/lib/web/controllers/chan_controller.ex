1d4aae4280c12f8cad9307a5e7ffd1536c38b681
Théophile Choutri <theophile@choutri.eu>
2017-12-04 11:48:14 +0100

    404 !

--- apps/web/lib/web/controllers/chan_controller.ex
+++ apps/web/lib/web/controllers/chan_controller.ex
@@ -2,6 +2,12 @@ defmodule Web.ChanController do
   use Web, :controller
 
   def show(conn, %{"slug" => slug}) do
-    render conn, "chan.html", chan: Core.get_links({:chan, slug})  
+    if chan=Core.get_links({:chan, slug}) do
+      render conn, "chan.html", chan: chan
+    else
+      conn
+      |> put_status(404)
+      |> render("404.html")
+    end
   end
 end
