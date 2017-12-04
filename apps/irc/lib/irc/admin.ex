bd13b2d05bc334792cf941c531191b5be6da77f2
Théophile Choutri <theophile@choutri.eu>
2017-12-04 11:22:59 +0100

    meeeeeh

--- apps/irc/lib/irc/admin.ex
+++ apps/irc/lib/irc/admin.ex
@@ -9,7 +9,7 @@ defmodule IRC.Admin do
 
   def parse("url " <> channel, username) do
     case Core.gib_slug(channel, username) do
-      {:ok, slug} -> Web.Router.Helpers.chan_url(Web.Endpoint, :show, slug)
+      {:ok, slug} -> {:ok, Web.Router.Helpers.chan_url(Web.Endpoint, :show, slug)}
       {:error, :nochan}  -> {:ok, "Bien essayé, tocard…"}
       {:error, :noadmin} -> {:ok, "Si t'es pas admin tu vas pas aller bien loin…"}
     end
