691236575a65b9b207af3c89fa1d916b5e25a425
Théophile Choutri <theophile@choutri.eu>
2017-12-04 16:27:02 +0100

    move it

---
+++ config/dev.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+config :irc, IRC.State,
+  host: "127.0.0.1",
+  realname: "Ἑκάτερος",
+  nickname: "Hecateros",
+  pass: "",
+  port: 6667,
+  tls?: false,
+  username: "Hecateros",
+  channels: []
+
