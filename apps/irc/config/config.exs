08b09b8953f74e61bc4a2a2eec75a3c7932048b4
Théophile Choutri <theophile@choutri.eu>
2017-12-04 16:28:57 +0100

    blo

--- apps/irc/config/config.exs
+++ apps/irc/config/config.exs
@@ -1,12 +1,1 @@
 use Mix.Config
-
-config :irc, IRC.State,
-  # host: "irc.enst.rezosup.org",
-  host: "127.0.0.1",
-  realname: "Ἑκάτερος",
-  nickname: "Hecateros",
-  pass: "",
-  port: 6667, 
-  tls?: false,
-  username: "Hecateros",
-  channels: []
