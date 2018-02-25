3facc5ed9781f3558551caf1edbe0236748fa4a9
Théophile Choutri <theophile@choutri.eu>
2018-02-25 20:46:59 +0100

    bring the config back

---
+++ apps/web/config/prod.exs
@@ -0,0 +1,21 @@
+use Mix.Config
+
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with brunch.io to recompile .js and .css sources.
+config :web, Web.Endpoint,
+  load_from_system_env: true,
+  url: [host: System.get_env("HOSTNAME"), port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: "apps/web/"
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "$time $metadata[$level] $message\n"
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
